8152cb9edd577a81f32a04e9052763a47d89361b
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2026-04-17 14:42:10 +0200

    css fixes

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,minimum-scale=1">
 	<title>@yield('title', 'Login System')</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet" />
 	@vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,6 +23,9 @@
 	<meta name="twitter:description"
 		content="@yield('meta_description', 'A developer who loves building things for the web. Thoughts on code, design, and everything in between.')">
 
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet" />
 	@stack('schema')
 	@vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
