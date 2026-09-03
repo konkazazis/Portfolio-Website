8ba05e8ae6d7b7033b5ed4ec33c7f99656646018
Konstantinos Kazazis <kazaziskonstantinos@proton.me>
2026-09-03 18:59:08 +0200

    quill-highlight fix

--- resources/js/highlight.js
+++ resources/js/highlight.js
@@ -1,4 +1,12 @@
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github-dark-dimmed.css';
 
-document.addEventListener('DOMContentLoaded', () => hljs.highlightAll());
+const highlightCodeBlocks = () => {
+	document.querySelectorAll('pre.ql-syntax, pre code').forEach((block) => {
+		if (!block.classList.contains('hljs')) {
+			hljs.highlightElement(block);
+		}
+	});
+};
+
+document.addEventListener('DOMContentLoaded', highlightCodeBlocks);
