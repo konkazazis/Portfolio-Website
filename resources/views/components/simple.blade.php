adfb6fa7d41dbb7aa83f1c6197138167b21def8c
Konstantinos Kazazis <kazaziskonstantinos@proton.me>
2026-09-08 19:41:01 +0200

    fixed login page

--- resources/views/components/simple.blade.php
+++ resources/views/components/simple.blade.php
@@ -1,6 +1,13 @@
--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 <body class="admin-flat min-h-screen bg-white antialiased dark:bg-zinc-950">
-    <div class="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div class="flex min-h-svh flex-col items-center justify-center lg:p-2 md:p-10">
         <div class="flex w-full max-w-sm flex-col gap-2">
             <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium mb-2">
                 <div class="w-10 h-10 bg-zinc-900 flex items-center justify-center">
